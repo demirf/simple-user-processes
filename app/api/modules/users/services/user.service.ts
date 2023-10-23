a3224fa797b3f958f041c887a1a97d2d30bb0446
Ferhat Demir <edemirferhat@gmail.com>
2023-10-23 03:58:26 +0300

    remove log

--- app/api/modules/users/services/user.service.ts
+++ app/api/modules/users/services/user.service.ts
@@ -1,10 +1,9 @@
-import { Injectable, PreconditionFailedException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from '../entities/user.entity';
 import { UserCreateDto } from '../dto/user.create.dto';
 import { UserFilterDto } from '../dto/user.filter.dto';
-import { validate } from 'class-validator';
 
 @Injectable()
 export class UserService {
@@ -16,26 +15,19 @@ export class UserService {
   async create(createDto: UserCreateDto) {
     const user = new User();
     user.name = createDto.name;
-    console.log(user);
+
     return this.usersRepository.save(user);
   }
 
-  findAll() {
+  async findAll() {
     return this.usersRepository.find();
   }
 
-  findOne(filterDto: UserFilterDto) {
+  async findOne(filterDto: UserFilterDto) {
     return this.usersRepository.findBy(filterDto);
   }
 
   async remove(id: number) {
     await this.usersRepository.softDelete(id);
-  }
-
-  async validate() {
-    const errors = await validate(this);
-    if (errors.length > 0) {
-      throw new PreconditionFailedException(errors);
-    }
   }
 }
